7555c9487ca2be7c1a276e4973d95fb41ab8cd03
Adrian Moring <adriantmoringbass@gmail.com>
2026-05-26 12:40:55 -0400

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <AudioProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/ambiently">
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sound/:id" element={<SoundEnviroment />} />
